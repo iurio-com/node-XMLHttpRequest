57e6048122bcf270d605407582d501c2edd3d6c3
Tobias Kellner <imcybot@gmail.com>
2026-06-11 12:50:12 +0200

    feat: Support sending FormData instances

--- tests/server.js
+++ tests/server.js
@@ -77,7 +77,7 @@ var server = http.createServer(function (req, res) {
                 res.end();
             }
             else {
-                res.writeHead(404, {"Content-Type": "text/plain"})
+                res.writeHead(404, {"Content-Type": "text/plain"});
                 res.end("Not found");
             }
     }
